af6e7f48b48e239761fb5b07b6a721cba20938e1
anoam <anoam@yandex.ru>
2018-01-28 21:24:13 +0300

    Logger

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,5 +9,13 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_conf.fetch("connection_string") }
+  config.error_handlers << ->(ex, ctx_hash) do
+    logger = Logger.new(Rails.root.join("log/sending_#{Rails.env}.log"))
+    logger.error(<<~LOG)
+      Exception was raised: #{ex}.
+      Details: #{ctx_hash}
+    LOG
+
+  end
 end
 
